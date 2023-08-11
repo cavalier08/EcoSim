f263d9227d78ee402ce142737881884de94e5d20
AndiJingzhiTse <103150017+AndiJingzhiXie@users.noreply.github.com>
2023-08-11 15:47:54 -0400

    Add files via upload

---
+++ Animals.js
@@ -0,0 +1,23 @@
+num =0;
+num= 1;
+console.log(num*2);
+
+
+class Heron {
+    foodRequiredPerDay=0.1;
+    amountOfMeat=5;
+    foodRemainedInStomache=4;
+    foodThreshold=0.7;
+    caughtPreyProbability=0.8;
+    foodEatonSinceTheLastReproduction=0;
+    reproductionThreshold=12;
+
+  constructor() {
+
+  }
+
+   die(){
+    destroy();
+  }
+
+}
