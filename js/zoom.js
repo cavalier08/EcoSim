829f6d40d90de36ac2467bdf7ea986ecc4eff293
joc31yn <140670138+joc31yn@users.noreply.github.com>
2023-08-12 16:12:30 -0400

    Update zoom.js

--- js/zoom.js
+++ js/zoom.js
@@ -3,17 +3,21 @@ var height = 150;
 var addition = 2;
 var intervalID = 0;
 
-function increaseImage(id) {
+function increaseImage(id, des, count) {
+    document.getElementById(des).style.display='block';
+    document.getElementById(count).style.display='none';
     clearInterval(intervalID);
     intervalID = setInterval(function () { zoomInImage(id) }, 1);
 }
-function decreaseImage(id) {
+function decreaseImage(id, des, count) {
+    document.getElementById(des).style.display='none';
+    document.getElementById(count).style.display='block';
     clearInterval(intervalID);
     intervalID = setInterval(function () { zoomOutImage(id) }, 1);
 }
 
 function zoomInImage(id) {
-    if (width < 200) {
+    if (width < 180) {
         width += addition;
         height += addition;
         document.getElementById(id).style.width = width;
