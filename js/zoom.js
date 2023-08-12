f0d68d2db2df6e80b7444bb6ba627938f08d2d76
joc31yn <140670138+joc31yn@users.noreply.github.com>
2023-08-12 01:16:40 -0400

    Update zoom.js

--- js/zoom.js
+++ js/zoom.js
@@ -1,31 +1,36 @@
 var width = 150;
-var addition = 1;
+var height = 150;
+var addition = 2;
 var intervalID = 0;
 
-function increaseImage(id){
+function increaseImage(id) {
     clearInterval(intervalID);
-    intervalID = setInterval(function() {zoomInImage(id)}, 1);
+    intervalID = setInterval(function () { zoomInImage(id) }, 1);
 }
-function decreaseImage(id){
+function decreaseImage(id) {
     clearInterval(intervalID);
-    intervalID = setInterval(function() {zoomOutImage(id)}, 1);
+    intervalID = setInterval(function () { zoomOutImage(id) }, 1);
 }
 
-function zoomInImage(id){
-    if(width<200){
+function zoomInImage(id) {
+    if (width < 200) {
         width += addition;
-        document.getElementById(id).style.width = width; 
+        height += addition;
+        document.getElementById(id).style.width = width;
+        document.getElementById(id).style.height = height;
         console.log(width);
-    } else{
+    } else {
         clearInterval(intervalID);
     }
 }
-function zoomOutImage(id){
-    if(width>150){
+function zoomOutImage(id) {
+    if (width > 150) {
         width -= addition;
-        document.getElementById(id).style.width = width; 
+        height -= addition;
+        document.getElementById(id).style.width = width;
+        document.getElementById(id).style.height = height;
         console.log(width);
-    } else{
+    } else {
         clearInterval(intervalID);
     }
 }
