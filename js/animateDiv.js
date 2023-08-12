72dd28d1ca038537954dd7915e762d45ca2bc400
cavalier08 <138271110+cavalier08@users.noreply.github.com>
2023-08-12 11:00:36 -0400

    Add files via upload

---
+++ js/animateDiv.js
@@ -0,0 +1,17 @@
+function myMove() {
+    let id=null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos ++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+
+}
